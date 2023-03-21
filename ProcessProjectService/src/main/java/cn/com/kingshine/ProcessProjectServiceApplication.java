f5345d524660e7dc32e45707cd2f8720c693e353
zhangfx <zhangfuxing1010@163.com>
2023-03-21 14:36:44 +0800

    基本微服务模板，包含网关、注册中心、配置中心

---
+++ ProcessProjectService/src/main/java/cn/com/kingshine/ProcessProjectServiceApplication.java
@@ -0,0 +1,23 @@
+package cn.com.kingshine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author zhangfx
+ * @date 2023/02/21
+ */
+@RefreshScope
+@RestController
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients
+public class ProcessProjectServiceApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(ProcessProjectServiceApplication.class, args);
+	}
+}
---
+++ gateway/src/main/java/cn/com/kingshine/GatewayApplication.java
@@ -0,0 +1,15 @@
+package cn.com.kingshine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author zhangfx
+ * @date 2023/3/21
+ */
+@SpringBootApplication
+public class GatewayApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayApplication.class, args);
+	}
+}
